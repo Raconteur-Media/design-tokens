a8ab20d2046a5ef0571e3e97b656400f194ba217
Nicola Pernice <k@w2b.it>
2020-04-01 15:00:15 +0100

    Adds dist folder

---
+++ dist/index.js
@@ -0,0 +1,177 @@
+export default {
+    "space": [
+        0,
+        4,
+        8,
+        16,
+        24,
+        32,
+        40
+    ],
+    breakpoints: ['22.5em','37.5em', '90em',],
+    sizes: {
+        maxWidth: '1050px',
+        maxWidthCentered: '650px',
+    },
+    "fonts": {
+        body: '"Apercu Pro", system-ui, -apple-system, sans-serif',
+        heading: '"Tiempos Headline", Georgia, serif',
+        monospace: 'monospace'
+    },
+    "fontSizes": [
+        14,
+        16,
+        18,
+        22,
+        24,
+        34,
+        48,
+        60,
+        96
+    ],
+    "fontWeights": {
+        body: 400,
+        heading: 300,
+        bold: 800
+    },
+    "lineHeights": {
+        body: 1.414,
+        heading: 1.333,
+        subtitle: 1.125
+    },
+    "text": {
+        "caption": {
+            fontWeight: 'bold',
+            fontSize: 0,
+        },
+        "caps": {
+            textTransform: 'uppercase',
+            letterSpacing: '0.1em',
+        },
+        "subsans": {
+            fontFamily: '"Apercu Pro", system-ui, -apple-system, sans-serif',
+            lineHeight: 1.125,
+            fontWeight: 800,
+            fontSize: 1,
+        },
+        "subserif": {
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 300,
+            fontSize: 4,
+        },
+    },
+    "colors": {
+        text: '#000',
+        background: '#fff',
+        primary: '#e64150',
+        secondary: '#055aa0',
+        muted: '#00a79f',
+        "raconteurMidGrey": "#979797",
+        "raconteurSoftGrey": "#f1f1f1",
+        "raconteurPink": "#f08791",
+        "raconteurAqua": "#78c8d7",
+        modes: {
+            dark: {
+                text: '#fff',
+                background: '#000',
+                primary: '#e64150',
+                secondary: '#055aa0',
+                muted: '#111',
+            },
+        }
+    },
+    "styles": {
+        root: {
+            fontFamily: '"Apercu Pro", system-ui, -apple-system, sans-serif',
+            lineHeight: 1.414,
+            fontWeight: 400,
+            fontSize: 1
+        },
+        h1: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 9
+        },
+        h2: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 8
+        },
+        h3: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 7
+        },
+        h4: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 6
+      },
+        h5: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 5
+        },
+        h6: {
+            color: '#000',
+            fontFamily: '"Tiempos Headline", Georgia, serif',
+            lineHeight: 1.333,
+            fontWeight: 800,
+            fontSize: 3
+        },
+        p: {
+            color: '#000',
+            fontFamily: '"Apercu Pro", system-ui, -apple-system, sans-serif',
+            fontWeight: 400,
+            lineHeight: 1.333,
+            fontSize: 3
+        },
+        a: {
+            color: '#e64150',
+        },
+        blockquote: {
+
+        },
+        small: {
+            fontSize: 0
+        },
+        pre: {
+            fontFamily: 'monospace',
+            overflowX: 'auto',
+            code: {
+                color: 'inherit'
+            }
+        },
+        code: {
+            fontFamily: 'monospace',
+            fontSize: 'inherit'
+        },
+        table: {
+            width: '100%',
+            borderCollapse: 'separate',
+            borderSpacing: 0
+        },
+        th: {
+            textAlign: 'left',
+            borderBottomStyle: 'solid'
+        },
+        td: {
+            textAlign: 'left',
+            borderBottomStyle: 'solid'
+        },
+        img: {
+            maxWidth: '100%'
+        }
+  }
+};
---
+++ dist/rac-dsm-theme.js
@@ -0,0 +1,425 @@
+export default {
+  "colors": {
+    "raconteurRed": "#e64150",
+    "raconteurGreen": "#00a79f",
+    "raconteurPink": "#f08791",
+    "raconteurBlue": "#78c8d7",
+    "raconteurRoyalBlue": "#055aa0",
+    "raconteurBlack": "#000000",
+    "raconteurMidGrey": "#979797",
+    "raconteurSoftGrey": "#f1f1f1"
+  },
+  "fontSizes": [12, 14, 16, 18, 22, 24, 34, 48, 60, 96],
+  "fonts": {
+    "h1": "Tiempos Headline",
+    "h3": "Tiempos Headline",
+    "h2": "Tiempos Headline",
+    "body1": "Apercu Pro",
+    "caption": "Apercu Pro",
+    "subtitle1": "Tiempos Headline",
+    "subtitle2": "Apercu Pro",
+    "h4": "Tiempos Headline",
+    "caption2": "Apercu Pro",
+    "h5": "Tiempos Headline",
+    "body2": "Apercu Pro",
+    "button": "Apercu Pro",
+    "h6": "Tiempos Headline",
+    "h7": "Tiempos Headline",
+    "h1White": "Tiempos Headline",
+    "h2White": "Tiempos Headline",
+    "h3White": "Tiempos Headline",
+    "h4White": "Tiempos Headline",
+    "h5White": "Tiempos Headline",
+    "h6White": "Tiempos Headline",
+    "h7White": "Tiempos Headline",
+    "subtitleWhite": "Tiempos Headline",
+    "subtitle2White": "Apercu Pro",
+    "body1White": "Apercu Pro",
+    "body2White": "Apercu Pro",
+    "captionWhite": "Apercu Pro",
+    "caption2White": "Apercu Pro",
+    "body2WhiteRegular": "Apercu Pro",
+    "subtitleWhite18Pt": "Tiempos Headline",
+    "subtitleBlack18Pt": "Tiempos Headline",
+    "caption2Black": "Apercu Pro"
+  },
+  "lineHeights": {
+    "h1": "65px",
+    "h3": "50px",
+    "h2": "46px",
+    "body1": "26px",
+    "caption": "25px",
+    "subtitle1": "26px",
+    "subtitle2": "16px",
+    "h4": "36px",
+    "caption2": "25px",
+    "h5": "30px",
+    "body2": "18px",
+    "button": "13px",
+    "h6": "22px",
+    "h7": "20px",
+    "h1White": "65px",
+    "h2White": "46px",
+    "h3White": "50px",
+    "h4White": "36px",
+    "h5White": "30px",
+    "h6White": "22px",
+    "h7White": "20px",
+    "subtitleWhite": "26px",
+    "subtitle2White": "16px",
+    "body1White": "26px",
+    "body2White": "18px",
+    "captionWhite": "25px",
+    "caption2White": "25px",
+    "body2WhiteRegular": "18px",
+    "subtitleWhite18Pt": "20px",
+    "subtitleBlack18Pt": "20px",
+    "caption2Black": "18px"
+  },
+  "fontWeights": [300, 400, 500, 600, 800, "bold"],
+  "textStyles": {
+    "h1": {
+      "fontSize": "96px",
+      "lineHeight": "65px",
+      "textAlign": "left",
+      "color": "#000000",
+      "letterSpacing": "1.92px",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h3": {
+      "fontSize": "48px",
+      "lineHeight": "50px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h2": {
+      "fontSize": "60px",
+      "lineHeight": "46px",
+      "textAlign": "left",
+      "color": "#000000",
+      "letterSpacing": "1.2px",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "body1": {
+      "fontSize": "18px",
+      "lineHeight": "26px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "caption": {
+      "fontSize": "14px",
+      "lineHeight": "25px",
+      "textAlign": "left",
+      "color": null,
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "subtitle1": {
+      "fontSize": "22px",
+      "lineHeight": "26px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "subtitle2": {
+      "fontSize": "16px",
+      "lineHeight": "16px",
+      "textAlign": "left",
+      "color": "#000000",
+      "letterSpacing": "0.57px",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "h4": {
+      "fontSize": "34px",
+      "lineHeight": "36px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "caption2": {
+      "fontSize": "14px",
+      "lineHeight": "25px",
+      "textAlign": "right",
+      "color": null,
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "h5": {
+      "fontSize": "24px",
+      "lineHeight": "30px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "body2": {
+      "fontSize": "16px",
+      "lineHeight": "18px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "button": {
+      "fontSize": "12px",
+      "lineHeight": "13px",
+      "textAlign": "center",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "h6": {
+      "fontSize": "18px",
+      "lineHeight": "22px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h7": {
+      "fontSize": "16px",
+      "lineHeight": "20px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h1White": {
+      "fontSize": "96px",
+      "lineHeight": "65px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "letterSpacing": "1.92px",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h2White": {
+      "fontSize": "60px",
+      "lineHeight": "46px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "letterSpacing": "1.2px",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h3White": {
+      "fontSize": "48px",
+      "lineHeight": "50px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h4White": {
+      "fontSize": "34px",
+      "lineHeight": "36px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h5White": {
+      "fontSize": "24px",
+      "lineHeight": "30px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h6White": {
+      "fontSize": "18px",
+      "lineHeight": "22px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "h7White": {
+      "fontSize": "16px",
+      "lineHeight": "20px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "500",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "subtitleWhite": {
+      "fontSize": "22px",
+      "lineHeight": "26px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "subtitle2White": {
+      "fontSize": "16px",
+      "lineHeight": "16px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "letterSpacing": "0.57px",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "body1White": {
+      "fontSize": "18px",
+      "lineHeight": "26px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "letterSpacing": "0.47px",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "body2White": {
+      "fontSize": "16px",
+      "lineHeight": "18px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "captionWhite": {
+      "fontSize": "14px",
+      "lineHeight": "25px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "caption2White": {
+      "fontSize": "14px",
+      "lineHeight": "25px",
+      "textAlign": "right",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "body2WhiteRegular": {
+      "fontSize": "16px",
+      "lineHeight": "18px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "400",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    },
+    "subtitleWhite18Pt": {
+      "fontSize": "16px",
+      "lineHeight": "20px",
+      "textAlign": "left",
+      "color": "#f1f1f1",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "subtitleBlack18Pt": {
+      "fontSize": "16px",
+      "lineHeight": "20px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "300",
+      "fontFamily": "Tiempos Headline",
+      "backgroundColor": null
+    },
+    "caption2Black": {
+      "fontSize": "14px",
+      "lineHeight": "18px",
+      "textAlign": "left",
+      "color": "#000000",
+      "fontStyle": "normal",
+      "fontWeight": "bold",
+      "fontFamily": "Apercu Pro",
+      "backgroundColor": null
+    }
+  },
+  "colorStyles": {
+    "raconteurRed": {
+      "color": "#e64150"
+    },
+    "raconteurGreen": {
+      "color": "#00a79f"
+    },
+    "raconteurPink": {
+      "color": "#f08791"
+    },
+    "raconteurBlue": {
+      "color": "#78c8d7"
+    },
+    "raconteurRoyalBlue": {
+      "color": "#055aa0"
+    },
+    "raconteurBlack": {
+      "color": "#000000"
+    },
+    "raconteurMidGrey": {
+      "color": "#979797"
+    },
+    "raconteurSoftGrey": {
+      "color": "#f1f1f1"
+    }
+  }
+};
